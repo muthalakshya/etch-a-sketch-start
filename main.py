bfe685daa00e83539b2d75c144b25e5dbbde1add
muthalakshya <120473291+muthalakshya@users.noreply.github.com>
2023-02-22 21:44:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.backward(10)
+
+def move_left():
+    tim.left(10)
+
+def move_right():
+    tim.right(10)
+
+def move_clear_drawing():
+    tim.penup()
+    tim.clear()
+    tim.home()
+    tim.pendown()
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=move_left)
+screen.onkey(key="d", fun=move_right)
+screen.onkey(key="c", fun=move_clear_drawing)
+screen.exitonclick()
